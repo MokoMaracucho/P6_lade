16e3dd2de51bfa31562b838d6b0f400852c1b912
MokoMaracucho <desmoulin.federico@gmail.com>
2021-06-06 21:18:24 +0200

    Spring Initializr

---
+++ src/main/java/com/oc/p6_lade/P6LadeApplication.java
@@ -0,0 +1,13 @@
+package com.oc.p6_lade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P6LadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P6LadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oc/p6_lade/P6LadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oc.p6_lade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P6LadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
